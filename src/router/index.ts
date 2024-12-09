97939559198eb7a769a038e9a6ce502e105b8caa
formosawqd <formosadong@gmail.com>
2024-12-09 17:45:45 +0800

    删除无用组件

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,17 +6,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: () => import('../views/AboutView.vue')
-    },
-    {
-      path: '/test',
-      name: 'test',
-      component: () => import('../views/TestView.vue')
+      component: () => import('../views/home/HomeView.vue')
     }
   ]
 })
--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
+    open: true,
     proxy: {
       '/api': {
         target: 'http://localhost:3000',
